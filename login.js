04700a049fcf20de7b31075714bd2f5ab9cb1eaf
Origami404 <Origami404@foxmail.com>
2021-09-21 11:39:02 +0800

    Clearup

--- login.js
+++ login.js
@@ -42,46 +42,46 @@ async function login(){
             challenge = challenge.substr(2);
             challenge = challenge.substr(0,challenge.length-2);
 			
-		var i = info({
-			username: username,
-			password: password,
-			ip: ip,
-			acid: '1',
-			enc_ver: 'srun_bx1'
-		    }, challenge);
+			var i = info({
+				username: username,
+				password: password,
+				ip: ip,
+				acid: '1',
+				enc_ver: 'srun_bx1'
+			}, challenge);
 
-		var hmd5 = md5(password,challenge);
+			var hmd5 = md5(password,challenge);
 
-		var chksum = challenge + username;
-		chksum += challenge + hmd5;
-		chksum += challenge + '1';
-		chksum += challenge + ip;
-		chksum += challenge + '200';
-		chksum += challenge + '1';
-		chksum += challenge + i;
+			var chksum = challenge + username;
+			chksum += challenge + hmd5;
+			chksum += challenge + '1';
+			chksum += challenge + ip;
+			chksum += challenge + '200';
+			chksum += challenge + '1';
+			chksum += challenge + i;
 
-		chksum = sha1(chksum);
-		i = encodeURIComponent(i);
-		hmd5 = encodeURIComponent('{MD5}'+hmd5);
+			chksum = sha1(chksum);
+			i = encodeURIComponent(i);
+			hmd5 = encodeURIComponent('{MD5}'+hmd5);
 
-		fetch("http://10.248.98.2/cgi-bin/srun_portal?callback="+callback+Date.parse(new Date())+"&action=login&username="+username+"&password="+hmd5+"&ac_id=1&ip="+ip+"&chksum="+chksum+"&info="+i+"&n=200&type=1&os=Windows+10&name=Windows&double_stack=0&_=" + Date.parse(new Date()), {
-		    "headers": {
-			"accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
-			"accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
-			"proxy-connection": "keep-alive",
-			"x-requested-with": "XMLHttpRequest",
-		    },
-		    "referrer": "http://10.248.98.2/srun_portal_pc?ac_id=1&theme=basic2",
-		    "referrerPolicy": "strict-origin-when-cross-origin",
-		    "body": null,
-		    "method": "GET",
-		    "mode": "cors"
-		}).then(res=>{
-			if(res.ok)return res.text();
-			else console.log("err");
-		}).then(text=>{
-			console.log(text);
-		});
+			fetch("http://10.248.98.2/cgi-bin/srun_portal?callback="+callback+Date.parse(new Date())+"&action=login&username="+username+"&password="+hmd5+"&ac_id=1&ip="+ip+"&chksum="+chksum+"&info="+i+"&n=200&type=1&os=Windows+10&name=Windows&double_stack=0&_=" + Date.parse(new Date()), {
+				"headers": {
+				"accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+				"accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
+				"proxy-connection": "keep-alive",
+				"x-requested-with": "XMLHttpRequest",
+				},
+				"referrer": "http://10.248.98.2/srun_portal_pc?ac_id=1&theme=basic2",
+				"referrerPolicy": "strict-origin-when-cross-origin",
+				"body": null,
+				"method": "GET",
+				"mode": "cors"
+			}).then(res=>{
+				if(res.ok)return res.text();
+				else console.log("err");
+			}).then(text=>{
+				console.log(text);
+			});
         });
     });
 }
