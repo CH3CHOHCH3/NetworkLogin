e81b95571e0562021f06c2486311843bb870abe2
h0gan1ee <lihaojun2003@outlook.com>
2022-05-26 10:40:07 +0800

    feat: true detect

--- login.js
+++ login.js
@@ -4,6 +4,8 @@ import info from './cryptos/info.cjs';
 import fetch from 'node-fetch';
 import { execSync } from 'child_process';
 import pRetry from 'p-retry';
+import pTimeout from 'p-timeout';
+import dns from 'dns';
 
 // 忽略前两个参数, argv 是一个类似 ['node', 'login.js', 'xxx', 'xxx'] 的数组
 const [, , username, password] = process.argv;
@@ -12,7 +14,7 @@ const [, , username, password] = process.argv;
 // exec 的返回值是 Buffer 或者是 String, 二者都可以 toString, trim 以去掉最后的回车
 const ip = execSync("ip addr | grep -o 'inet 10.[^/]*' | grep -o '[0-9.]*'").toString().trim();
 // macOS 用户用这个：
-// const ip = execSync('ipconfig getifaddr en0').toString().trim();
+// const ip = execSync('ifconfig getifaddr en0').toString().trim();
 
 const callback = "jQuery112406199704547613489_";
 
@@ -23,26 +25,8 @@ function sha1(data) {
     .digest("hex");
 }
 
-function timeout(ms, promise) {
-  return new Promise((resolve, reject) => {
-    const timer = setTimeout(() => {
-      reject(new Error('TIMEOUT'))
-    }, ms)
-
-    promise
-      .then(value => {
-        clearTimeout(timer)
-        resolve(value)
-      })
-      .catch(reason => {
-        clearTimeout(timer)
-        reject(reason)
-      })
-  })
-}
-
 async function login() {
-  const response = await timeout(1000, fetch("http://10.248.98.2/cgi-bin/get_challenge?callback=" + callback + Date.parse(new Date()) + "&username=" + username + "&ip=" + ip + "&_=" + Date.parse(new Date()), {
+  const response = await pTimeout(fetch("http://10.248.98.2/cgi-bin/get_challenge?callback=" + callback + Date.parse(new Date()) + "&username=" + username + "&ip=" + ip + "&_=" + Date.parse(new Date()), {
     "headers": {
       "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
       "accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
@@ -54,7 +38,9 @@ async function login() {
     "body": null,
     "method": "GET",
     "mode": "cors"
-  }));
+  }), 1000);
+  // (console.log(response);
+  if (!response.ok) throw new Error(response.statusText);
   const challenge = (await response.text()).match(/"challenge":"(.*?)",/)[1];
 
   var i = info({
@@ -79,7 +65,7 @@ async function login() {
   i = encodeURIComponent(i);
   hmd5 = encodeURIComponent('{MD5}' + hmd5);
 
-  await timeout(2000, fetch("http://10.248.98.2/cgi-bin/srun_portal?callback=" + callback + Date.parse(new Date()) + "&action=login&username=" + username + "&password=" + hmd5 + "&ac_id=1&ip=" + ip + "&chksum=" + chksum + "&info=" + i + "&n=200&type=1&os=Windows+10&name=Windows&double_stack=0&_=" + Date.parse(new Date()), {
+  const result = await pTimeout(fetch("http://10.248.98.2/cgi-bin/srun_portal?callback=" + callback + Date.parse(new Date()) + "&action=login&username=" + username + "&password=" + hmd5 + "&ac_id=1&ip=" + ip + "&chksum=" + chksum + "&info=" + i + "&n=200&type=1&os=Windows+10&name=Windows&double_stack=0&_=" + Date.parse(new Date()), {
     "headers": {
       "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
       "accept-language": "en,zh-CN;q=0.9,zh;q=0.8",
@@ -93,9 +79,19 @@ async function login() {
     "mode": "cors"
   }).catch(err => {
     throw new Error(err.toString());
-  }).then(res => {
-    if (!res.ok) throw new Error(res.statusText);
-  }));
+  }), 2000);
+  // console.log(result);
+  if (!result.ok) throw new Error(result.statusText);
+
+  dns.lookup('www.aliyun.com', { family: 4 }, (err, address, family) => {
+    if (address.startsWith('10.') || err)
+      throw new Error('No connection.');
+  });
 }
 
-pRetry(login, { retries: 60 })
+pRetry(login, {
+  onFailedAttempt: error => {
+    console.log(`Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`);
+  },
+  retries: 60
+});
