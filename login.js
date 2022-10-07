0d37ae9bbed3b560c1f8079ba952d556b70608c0
h0gan1ee <lihaojun2003@outlook.com>
2022-10-07 14:51:37 +0800

    feat: several - Improve connectivity test - Add inpect flag support

--- login.js
+++ login.js
@@ -2,9 +2,16 @@ import crypto from 'crypto';
 import md5 from './cryptos/md5.cjs';
 import info from './cryptos/info.cjs';
 import fetch from 'node-fetch';
+import inspector from 'node:inspector';
 import pRetry from 'p-retry';
 import pTimeout from 'p-timeout';
 import { networkInterfaces } from 'os';
+
+const debug = inspector.url() !== undefined;
+
+if (debug) {
+    var st, et;
+}
 
 const getIp = () => {
   const nets = networkInterfaces();
@@ -35,26 +42,43 @@ function sha1(data) {
 }
 
 async function test_connectivity() {
-  const testRes = await pTimeout(fetch("http://secr.baidu.com/", {
-    "headers": {
-      "accept": "text/html",
-      "cache-control": "no-cache",
-      "pragma": "no-cache",
-      "upgrade-insecure-requests": "1",
-    },
-    "referrerPolicy": "strict-origin-when-cross-origin",
+  if (debug) {
+    st = performance.now();
+  }
+  const testRes = await pTimeout(fetch("https://passport.baidu.com/passApi/html/_blank.html", {
     "body": null,
     "method": "GET"
   }), 1000);
+  if (debug) {
+    et = performance.now();
+    console.log('test_connectivity time:', et - st);
+  }
+  // const testRes = await pTimeout(fetch("http://secr.baidu.com/", {
+  //   "headers": {
+  //     "accept": "text/html",
+  //     "cache-control": "no-cache",
+  //     "pragma": "no-cache",
+  //     "upgrade-insecure-requests": "1",
+  //   },
+  //   "referrerPolicy": "strict-origin-when-cross-origin",
+  //   "body": null,
+  //   "method": "GET"
+  // }), 1000);
   if (!testRes || !testRes.ok) throw new Error(result.statusText);
-  const testBody = (await testRes.text()).trimStart();
-  // console.log(testBody.substring(0, 20));
-  if (!(testRes.ok && (testBody.startsWith('<!DOCTYPE') || testBody.startsWith('<html')))) {
+  // const testBody = (await testRes.text()).trimStart();
+  const testBody = (await testRes.text()).trim()
+  if (debug) {
+    console.log("testBody:", testBody.substring(0, 20));
+  }
+  if (!(testRes.ok && testBody == '')) {
     throw new Error('No connection.');
   }
 }
 
 async function login() {
+  if (debug) {
+    st = performance.now();
+  }
   const response = await pTimeout(fetch("http://10.248.98.2/cgi-bin/get_challenge?callback=" + callback + Date.parse(new Date()) + "&username=" + username + "&ip=" + ip + "&_=" + Date.parse(new Date()), {
     "headers": {
       "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
@@ -67,8 +91,12 @@ async function login() {
     "body": null,
     "method": "GET",
     "mode": "cors"
-  }), 1000);
-  // console.log(response);
+  }), 800);
+  if (debug) {
+    et = performance.now();
+    console.log('get_challenge time:', et - st);
+    console.log('get_challenge response:', response);
+  }
   if (!response || !response.ok) throw new Error(response.statusText);
   const responseBody = await response.text();
   const matches = responseBody.match(/"challenge":"(.*?)",/);
@@ -97,6 +125,9 @@ async function login() {
   i = encodeURIComponent(i);
   hmd5 = encodeURIComponent('{MD5}' + hmd5);
 
+  if (debug) {
+    st = performance.now();
+  }
   const result = await pTimeout(fetch("http://10.248.98.2/cgi-bin/srun_portal?callback=" + callback + Date.parse(new Date()) + "&action=login&username=" + username + "&password=" + hmd5 + "&ac_id=1&ip=" + ip + "&chksum=" + chksum + "&info=" + i + "&n=200&type=1&os=Windows+10&name=Windows&double_stack=0&_=" + Date.parse(new Date()), {
     "headers": {
       "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
@@ -109,21 +140,29 @@ async function login() {
     "body": null,
     "method": "GET",
     "mode": "cors"
-  }), 2000);
-  // console.log(result);
+  }), 800);
+  if (debug) {
+    et = performance.now();
+    console.log('login time:', et - st);
+    console.log('login response:', result);
+  }
   if (!result || !result.ok) throw new Error(result.statusText);
 
   await test_connectivity();
 }
 
-// await login().catch(err => console.log(err));
-try {
-  await test_connectivity();
-} catch {
-  await pRetry(login, {
-    onFailedAttempt: err => {
-      console.log(`Attempt ${err.attemptNumber} failed. There are ${err.retriesLeft} retries left.`);
-    },
-    retries: 14
-  });
+if (debug) {
+  await test_connectivity().catch(err => console.log(err));
+  await login().catch(err => console.log(err));
+} else {
+  try {
+    await test_connectivity();
+  } catch {
+    await pRetry(login, {
+      onFailedAttempt: err => {
+        console.log(`Attempt ${err.attemptNumber} failed. There are ${err.retriesLeft} retries left.`);
+      },
+      retries: 14
+    });
+  }
 }
